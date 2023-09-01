21d2f2278ab0068991f4e21db41ef9f81475a960
Andrei Alexandrov <andrei.alxv@gmail.com>
2023-09-01 17:53:42 +0300

    changes to offset navigation, changing readMe imgs

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -18,7 +18,7 @@ export default function CustomNavbar() {
                     <Nav className="mx-auto nav-links d-flex flex-row justify-content-center align-items-center gap-3">
                         <Nav.Link as={Link} spy={true} to='skills' smooth={true} duration={60} offset={54}>Skills</Nav.Link>
                         <Nav.Link as={Link} spy={true} to='cards-collection' smooth={true} duration={60} offset={-60}>My Card</Nav.Link>
-                        <Nav.Link as={Link} spy={true} to='projects' smooth={true} duration={60} offset={-66}>Projects</Nav.Link>
+                        <Nav.Link as={Link} spy={true} to='projects' smooth={true} duration={60} offset={42}>Projects</Nav.Link>
                     </Nav>
                     <Nav className="ms-auto nav-btns d-flex flex-row justify-content-center align-items-center mt-3">
                         <Link spy={true} to='contact' smooth={true} duration={60} offset={115}>
