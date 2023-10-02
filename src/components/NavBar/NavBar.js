d10326b1506e9c7777e5eaf31f1e92c6ead2e050
Andrei Alexandrov <andrei.alxv@gmail.com>
2023-10-02 16:07:22 +0300

    hamburger menu on smaller screens - collapsed by default

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,37 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import Music from "../Music/Music";
-
 import ToggleTheme from "../ToggleTheme/ToggleTheme";
-
 import "./Navbar.scss";
 
 export default function CustomNavbar() {
-    return (
-        <Navbar expand="lg" className="navbar">
-            <Container>
-                <Navbar.Brand className="">
-                    <ToggleTheme />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mx-auto nav-links d-flex flex-row justify-content-center align-items-center gap-3">
-                        <Nav.Link as={Link} spy={true} to='skills' smooth={true} duration={60} offset={54}>Skills</Nav.Link>
-                        <Nav.Link as={Link} spy={true} to='cards-collection' smooth={true} duration={60} offset={-60}>My Card</Nav.Link>
-                        <Nav.Link as={Link} spy={true} to='projects' smooth={true} duration={60} offset={42}>Projects</Nav.Link>
-                    </Nav>
-                    <Music />
-                    <Nav className="ms-auto nav-btns d-flex flex-row justify-content-center align-items-center mt-3">
-                        <Link spy={true} to='contact' smooth={true} duration={60} offset={115}>
-                            <button className="nav-btn" aria-label="Contact me">Contact me</button>
-                        </Link>
-                        <a href="/pdf-files/Andrei_Alexandrov_CV.pdf" download aria-label="Download CV">
-                            <button className="nav-btn">Download CV</button>
-                        </a>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  const [expanded, setExpanded] = useState(true);
+
+  const toggleNavbar = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Navbar expand="lg" className="navbar" expanded={expanded}>
+      <Container>
+        <ToggleTheme />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleNavbar} />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto nav-links d-flex flex-row justify-content-center align-items-center gap-3">
+            <Nav.Link as={Link} spy={true} to="skills" smooth={true} duration={60} offset={54}>
+              Skills
+            </Nav.Link>
+            <Nav.Link as={Link} spy={true} to="cards-collection" smooth={true} duration={60} offset={-60}>
+              My Card
+            </Nav.Link>
+            <Nav.Link as={Link} spy={true} to="projects" smooth={true} duration={60} offset={42}>
+              Projects
+            </Nav.Link>
+          </Nav>
+          <Music />
+          <Nav className="ms-auto nav-btns d-flex flex-row justify-content-center align-items-center mt-3">
+            <Link spy={true} to="contact" smooth={true} duration={60} offset={115}>
+              <button className="nav-btn" aria-label="Contact me">
+                Contact me
+              </button>
+            </Link>
+            <a href="/pdf-files/Andrei_Alexandrov_CV.pdf" download aria-label="Download CV">
+              <button className="nav-btn">Download CV</button>
+            </a>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
