14de54e327e81ca301d72ac2e06d15846368704a
Andrei Alexandrov <andrei.alxv@gmail.com>
2023-12-14 23:57:30 +0200

    added new project and updated CV document + some minor styling improvements

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -14,6 +14,17 @@ import BuddySearch from "../../images/projects/sports_match/buddySearch.jpg"
 import ChatPage from "../../images/projects/sports_match/chatPage.jpg"
 import PlacesPage from "../../images/projects/sports_match/placesPages.jpg"
 
+//Cocktail site project photos
+import CocktailsStartingPage from "../../images/projects/cocktail_site/cocktails-starting-image.png"
+import CocktailsHomePage from "../../images/projects/cocktail_site/cocktails-home.png"
+import CocktailsFavoritesPage from "../../images/projects/cocktail_site/cocktails-favorites.png"
+import CocktailsDetailsPage from "../../images/projects/cocktail_site/cocktails-details.png"
+
+//Countdown game project photos
+import CountdownGame from "../../images/projects/countdown_game/countdownGame.png"
+import CountdownGameWin from "../../images/projects/countdown_game/countdown-game-win.png"
+import CountdownGameLoss from "../../images/projects/countdown_game/countdown-game-loss.png"
+
 //Adopt aliens project photos
 import LoginAlienPage from "../../images/projects/adopt_aliens/login.jpg"
 import HomeAlienPage from "../../images/projects/adopt_aliens/home.jpg"
@@ -22,10 +33,6 @@ import DonateAlienPage from "../../images/projects/adopt_aliens/donate.jpg"
 import LoginAlienPageTwo from "../../images/projects/adopt_aliens/login2.jpg"
 import Github from "../../images/projects/github.png"
 
-//Countdown game project photos
-import CountdownGame from "../../images/projects/countdown_game/countdownGame.png"
-import CountdownGameWin from "../../images/projects/countdown_game/countdown-game-win.png"
-import CountdownGameLoss from "../../images/projects/countdown_game/countdown-game-loss.png"
 
 import "./Projects.scss";
 import "swiper/css";
@@ -37,6 +44,8 @@ export default function Projects() {
     const swiperRefProjectOne = useRef(null);
     const swiperRefProjectTwo = useRef(null);
     const swiperRefProjectThree = useRef(null);
+    const swiperRefProjectFour = useRef(null);
+
     const theme = useContext(themeContext);
     const darkMode = theme.state.darkMode;
 
@@ -57,6 +66,7 @@ export default function Projects() {
                         <div className="project-header">
                             <span style={{ color: darkMode ? "white" : "black" }}>My</span> Projects
                         </div>
+
                         {/* Project 1 */}
                         <div className="project-title">1. Sports Match</div>
                         <div className={darkMode ? "portfolio-project-text" : "portfolio-project-text-light"}>
@@ -66,16 +76,16 @@ export default function Projects() {
                             <br />
                             ---
                             <br />
-                            <span style={{ color: "white" }}>Used:</span>
-                            <span style={{ color: "#fba51e" }}> Javascript</span>,
-                            <span style={{ color: "#129dca" }}> React.js</span>,
-                            <span style={{ color: "#ce6b9c" }}> Sass</span>
+                            <span style={{ color: "white" }}>Used:&nbsp;</span>
+                            <span style={{ color: "#fba51e" }}>Javascript</span>,
+                            <span style={{ color: "#129dca" }}>&nbsp;React.js</span>,
+                            <span style={{ color: "#ce6b9c" }}>&nbsp;Sass</span>
                             <br />
-                            <span style={{ color: "white" }}>Additional: </span> react-bootstrap, react router,
+                            <span style={{ color: "white" }}>Additional:&nbsp; </span> react-bootstrap, react router,
                             emoji-picker-react, sweetalert2, react-icons, AJAX, local/session storage. <br />
                             Possible chat between 2 users in 2 different tabs from the same browser. <br />
                         </div>
-
+                        <br></br>
                         <div className="link-content">Source code:
                             <a href="https://www.github.com/andrei-alexandrov/sports-match" target='_blank' rel="noreferrer">
                                 <img src={Github} alt="Github" />
@@ -97,28 +107,33 @@ export default function Projects() {
                         <Arrows swiperRef={swiperRefProjectOne} />
 
                         {/* Project 2 */}
-                        <div className="project-title">2. Adopt Alien Creatures</div>
+                        <div className="project-title">2. Cocktail site</div>
                         <div className={darkMode ? "portfolio-project-text" : "portfolio-project-text-light"}>
-                            A small project for adopting alien creatures. You can adopt/leave an alien, or
-                            donate to support it. Animated background on most pages.
+                            Find interesting cocktails and get their recipes. If you like a specific one, add it to favorites.
                             <br />
                             ---
                             <br />
-                            Used: <span style={{ color: "#fba51e" }}> Javascript</span>, MVC pattern</div>
-
+                            <span style={{ color: "white" }}>Used:&nbsp;</span>
+                            <span style={{ color: "#fba51e" }}>Javascript</span>,
+                            <span style={{ color: "#129dca" }}>&nbsp;React.js</span>,
+                            <span style={{ color: "#7a50be" }}>&nbsp;Redux</span>,
+                            <span style={{ color: "#ce6b9c" }}>&nbsp;Sass</span>
+                            <br />
+                            <span style={{ color: "white" }}>Additional:&nbsp;</span>Redux thunk, react-router, gsap, react-boostrap, lottie-react.<br />
+                        </div>
+                        <br></br>
                         <div className="link-content">Source code:
-                            <a href="https://www.github.com/andrei-alexandrov/adopt-aliens" target='_blank' rel="noreferrer">
+                            <a href="https://github.com/andrei-alexandrov/cocktails-site" target='_blank' rel="noreferrer">
                                 <img src={Github} alt="Github" />
                             </a>
                         </div>
 
                         <SwiperComponent
                             images={[
-                                LoginAlienPage,
-                                HomeAlienPage,
-                                AdoptedAlienPage,
-                                DonateAlienPage,
-                                LoginAlienPageTwo
+                                CocktailsStartingPage,
+                                CocktailsHomePage,
+                                CocktailsFavoritesPage,
+                                CocktailsDetailsPage,
                             ]}
                             swiperRef={swiperRefProjectTwo}
                             enlargeImage={handleImageClick}
@@ -132,16 +147,16 @@ export default function Projects() {
                             <br />
                             ---
                             <br />
-                            Used:
-                            <span style={{ color: "#fba51e" }}> Javascript</span>,
-                            <span style={{ color: "#129dca" }}> React.js</span>,
-                            <span style={{ color: "#ce6b9c" }}> Sass</span>
+                            <span style={{ color: "white" }}>Used:&nbsp;</span>
+                            <span style={{ color: "#fba51e" }}>Javascript</span>,
+                            <span style={{ color: "#129dca" }}>&nbsp;React.js</span>,
+                            <span style={{ color: "#ce6b9c" }}>&nbsp;Sass</span>
                         </div>
 
                         <a href="https://countdown-timer-game.netlify.app/" target='_blank' rel="noreferrer">
                             <button className="project-3-play-btn">Play here</button>
                         </a>
-
+                        <br></br>
                         <div className="link-content">Source code:
                             <a href="https://github.com/andrei-alexandrov/countdown-game" target='_blank' rel="noreferrer">
                                 <img src={Github} alt="Github" />
@@ -160,20 +175,51 @@ export default function Projects() {
                         <Arrows swiperRef={swiperRefProjectThree} />
 
                         {/* Project 4 */}
+                        <div className="project-title">2. Adopt Alien Creatures</div>
+                        <div className={darkMode ? "portfolio-project-text" : "portfolio-project-text-light"}>
+                            A small project for adopting alien creatures. You can adopt/leave an alien, or
+                            donate to support it. Animated background on most pages.
+                            <br />
+                            ---
+                            <br />
+                            <span style={{ color: "white" }}>Used:&nbsp;</span>
+                            <span style={{ color: "#fba51e" }}>Javascript</span>,&nbsp;MVC pattern</div>
+                        <br></br>
+                        <div className="link-content">Source code:
+                            <a href="https://www.github.com/andrei-alexandrov/adopt-aliens" target='_blank' rel="noreferrer">
+                                <img src={Github} alt="Github" />
+                            </a>
+                        </div>
+
+                        <SwiperComponent
+                            images={[
+                                LoginAlienPage,
+                                HomeAlienPage,
+                                AdoptedAlienPage,
+                                DonateAlienPage,
+                                LoginAlienPageTwo
+                            ]}
+                            swiperRef={swiperRefProjectFour}
+                            enlargeImage={handleImageClick}
+                        />
+                        <Arrows swiperRef={swiperRefProjectFour} />
+
+                        {/* Project 5 */}
                         <div className="project-title">4. This portfolio</div>
                         <div className={darkMode ? "portfolio-project-text" : "portfolio-project-text-light"}>
                             ---
                             <br />
-                            <span style={{ color: "white" }}>Used:</span>
-                            <span style={{ color: "#fba51e" }}> Javascript</span>,
-                            <span style={{ color: "#129dca" }}> React.js</span>,
-                            <span style={{ color: "#ce6b9c" }}> Sass</span>
+                            <span style={{ color: "white" }}>Used:&nbsp;</span>
+                            <span style={{ color: "#fba51e" }}>Javascript</span>,
+                            <span style={{ color: "#129dca" }}>&nbsp;React.js</span>,
+                            <span style={{ color: "#ce6b9c" }}>&nbsp;Sass</span>
                             <br />
                             <span style={{ color: "white" }}>Additional: </span>
                             swiper, react-particles, emailJS, framer motion, react-scroll,
                             iconscout/react-unicorns, tsparticles, thecocktaildb API, react-lottie, gsap.
                             Fully responsive.
                         </div>
+                        <br></br>
                         <div className="link-content">Source code:
                             <a href="https://github.com/andrei-alexandrov/my-portfolio" target='_blank' rel="noreferrer">
                                 <img src={Github} alt="Github" />
